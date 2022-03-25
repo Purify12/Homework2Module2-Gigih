031d3fafa1fc5d42e17fa79dcd51930d82fff089
Purify12 <marchelsitumorang2000@gmail.com>
2022-03-25 15:59:16 +0700

    update tugas

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import './styles/style.css';
+import data from "./albumData/albumDummy";
+import AlbumList from "./components/album/albumList";
+import CreatePlaylist from "./components/playlist/createPlaylist";
+
+const SPOTIFY_CLIENT_ID = process.env;
+
+function App() {
+
+  useEffect(() => {
+    console.log(SPOTIFY_CLIENT_ID);
+    console.log(SPOTIFY_CLIENT_ID.SPOTIFY_CLIENT_SECRET)
+  }, []);
+
+  return (
+    <React.Fragment>
+      <CreatePlaylist />
+      <AlbumList value={data} />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/playlist/createPlaylist.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import FormCreatePlaylist from "./formCreatePlaylist";
+
+const CreatePlaylist = () => {
+    return (
+        <div className="createplaylist">
+            <div className="formcreate">
+            <h1>CREATE PLAYLIST</h1>      
+            <FormCreatePlaylist />
+            </div> 
+        </div>
+    )
+}
+
+export default CreatePlaylist;
