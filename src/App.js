aeb8f806d54124aecc2d3194016732c8c58210d9
Purify12 <marchelsitumorang2000@gmail.com>
2022-03-28 16:53:48 +0700

    homework3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './styles/style.css';
 import data from "./albumData/albumDummy";
 import AlbumList from "./components/album/albumList";
 import CreatePlaylist from "./components/playlist/createPlaylist";
+import TracksData from "./albumData/trackDummy";
+import TrackListCard from "./components/track/trackListCard";
+import TrackListTable from "./components/track/trackListTable";
 
 const SPOTIFY_CLIENT_ID = process.env;
 
@@ -17,6 +20,17 @@ function App() {
     <React.Fragment>
       <CreatePlaylist />
       <AlbumList value={data} />
+      <div className="container">
+      <div className="tracklist-table">
+        <h1>Tracklist</h1>
+        <TrackListTable value={TracksData}/>
+      </div>
+      <div className="albums-container">
+        {
+          TracksData.map((element) => <TrackListCard value={element} key={element.id} />)
+        }
+      </div>
+      </div>
     </React.Fragment>
   );
 }
